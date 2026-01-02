45661bad40dadb246ca1b695ae86b80fd0cf95b4
JkBoyo <jkmarhefka@gmail.com>
2026-01-01 22:04:56 -0500

    unmarshal user struct from login response

--- internal/jellyfin/jellyauth.go
+++ internal/jellyfin/jellyauth.go
@@ -6,8 +6,8 @@ import (
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
-	"net/http/httputil"
 	"os"
 )
 
@@ -19,7 +19,7 @@ func addJellyfinAuthHeader(r *http.Request, token, userName string) {
 		fmt.Sprintf(JellyfinAuthHeaderTemp,
 			token,
 			os.Getenv("RELEASE_VERSION"),
-			hex.EncodeToString([]byte(userName))+"12345",
+			hex.EncodeToString([]byte(userName))+os.Getenv("SERVER_ID"),
 		),
 	)
 	r.Header.Add("Content-Type", "application/json")
@@ -33,24 +33,23 @@ func AuthenticateUser(userName, password string, con context.Context) (string, e
 
 	client := http.DefaultClient
 
-	data, err := json.Marshal(params)
+	reqData, err := json.Marshal(params)
 	if err != nil {
 		return "", fmt.Errorf("Error marshaling params to json: %s", err.Error())
 	}
 
-	req, err := http.NewRequest("POST", fmt.Sprintf("https://%s/Users/authenticatebyname", os.Getenv("JELLYFIN_URL")), bytes.NewReader(data))
+	req, err := http.NewRequest("POST", fmt.Sprintf("https://%s/Users/authenticatebyname", os.Getenv("JELLYFIN_URL")), bytes.NewReader(reqData))
 	if err != nil {
 
 		return "", fmt.Errorf("Error creating request: %s", err.Error())
 	}
+	defer req.Body.Close()
 
 	addJellyfinAuthHeader(req, "", userName)
 
-	reqDat, err := httputil.DumpRequest(req, true)
-	if err != nil {
-		return "", fmt.Errorf("Error dumping the request: %s", err.Error())
-	}
-	fmt.Println(string(reqDat))
+	AuthResp := struct {
+		AccessToken string
+	}{}
 
 	resp, err := client.Do(req)
 	if err != nil {
@@ -58,13 +57,15 @@ func AuthenticateUser(userName, password string, con context.Context) (string, e
 	}
 	defer resp.Body.Close()
 
-	b, err := httputil.DumpResponse(resp, true)
+	respData, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return "", fmt.Errorf("Error dumping the response: %s", err.Error())
+		return "", err
 	}
-	fmt.Println(string(b))
 
-	token := "not gotten yet"
+	err = json.Unmarshal(respData, &AuthResp)
+	if err != nil {
+		return "", err
+	}
 
-	return token, nil
+	return AuthResp.AccessToken, nil
 }
